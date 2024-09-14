e18ca1fa92b46d86349b4079950b25524bc44501
ignacioct <ignaciotalaveracepeda@gmail.com>
2024-09-14 11:07:03 +0200

    Docker connection not working

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ with gr.Blocks() as demo:
     msg.submit(query_interface, [msg, chatbot], [msg, chatbot])
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
